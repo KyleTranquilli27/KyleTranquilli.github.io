5ecaac08931416c57fc93cdc569e9cb664c82b8b
KyleTranquilli27 <KyleTranquilli27@gmail.com>
2017-04-18 18:24:14 -0400

    gshuvdjhdf

--- sign/myscript.js
+++ sign/myscript.js
@@ -36,18 +36,55 @@ $(document).ready(function(){
         $('.attack').mouseover(function(){
     $('.hey5').fadeIn(1000);
 })
+         $('.attack').mouseover(function(){
+    $('.chaos').fadeIn(1000);
+})
+            $('.chaos').mouseover(function(){
+    $('.hey6').fadeIn(1000);
+})
+    $('.chaos').mouseover(function(){
+    $('.defend').fadeIn(4500);
+})
+      $('.castle').mouseover(function(){
+    $('.unicorn').fadeIn(4500);
+    $('.desc').fadeIn(4500);
+})
+      $('.defend').mouseover(function(){
+    $('.hey7').fadeIn(4500);
+    $('.castle').fadeIn(4500);
+    
+    
+})
     $(".bold").mouseover(function(){
     $(".hey3").animate({left: '37%'}, 1500);
+}); 
+     $(".chaos").mouseover(function(){
+    $(".hey6").animate({marginTop: '25%'}, 1500);
 }); 
     $(".surround").hover(function(){
       $('.hey3').css('color', 'rgba(245, 0, 0, 1)');
     },function(){
     $('.flyout').addClass('hidden');
 });
-       $(".attack").hover(function(){
-      $('.hey4').css('color', 'rgba(245, 0, 0, 1)');
+       $(".castle").hover(function(){
+      $('.hey7').css('color', 'rgba(245, 0, 0, 1)');
     },function(){
     $('.flyout').addClass('hidden');
 });
     
+    $(".defend").hover(function(){
+      $('.hey6').css('color', 'rgba(245, 0, 0, 1)');
+    },function(){
+    $('.flyout').addClass('hidden');
+});
+      $(".chaos").hover(function(){
+      $('.hey5').css('color', 'rgba(245, 0, 0, 1)');
+    },function(){
+    $('.flyout').addClass('hidden');
+});
+   $(".attack").hover(function(){
+      $('.hey4').css('color', 'rgba(245, 0, 0, 1)');
+    },function(){
+    $('.flyout').addClass('hidden');
+});
 });
