b642b54f3fb63e527c7af991df004e0e9571520b
KyleTranquilli27 <KyleTranquilli27@gmail.com>
2017-05-11 13:54:55 -0400

    hye

--- GET copy/myscript.js
+++ GET copy/myscript.js
@@ -1,37 +1,81 @@
 $(document).ready(function(){ 
     "use strict"; 
+var toggleBush=false; 
+var toggleClinton=false; 
+var toggleOJ=false; 
+var toggleBushsr=false;
 
-     $(".comedy").mouseenter(function(){
-      $(".textright").css("color", "rgba(245, 0, 0, 1)");
-      $(".textleft").css("opacity", "0");
-      $(".text").css("opacity", "0");
+     $(".Bush").click(function(){
+         toggleBush=!toggleBush;
+         if (toggleBush==true){
+              $(".Bush1").css("color", "blue");
+             $(".Bush1").css("opacity", "1");
+         }
+         
+         else{
+             $(".Bush1").css("opacity", "0");
+         }
+          
+});     
+     $(".Clinton").click(function(){
+         toggleClinton=!toggleClinton;
+         if (toggleClinton==true){
+               $(".Clinton1").css("color", "red");
+             $(".Clinton1").css("opacity", "1");
+              
+         }
+          else{
+              $(".Clinton1").css("opacity", "0");
+
+         }
+
 });
-    $(".comedy").mouseleave(function(){
-      $(".textright").css("color", "black");
-      $(".textleft").css("opacity", "1");
-      $(".text").css("opacity", "1");
+
+ $(".OJ").click(function(){
+         toggleClinton=!toggleClinton;
+         if (toggleClinton==true){
+               $(".OJ1").css("color", "green");
+             $(".OJ1").css("opacity", "1");
+              
+         }
+     else{
+              $(".OJ1").css("opacity", "0");
+
+         }
+
 });
-        $(".news").mouseenter(function(){
-      $(".textleft").css("color", "green");
-      $(".textright").css("opacity", "0");
-      $(".text").css("opacity", "0");
-});
-    $(".news").mouseleave(function(){
-      $(".textleft").css("color", "black");
-      $(".textright").css("opacity", "1");
-      $(".text").css("opacity", "1");
-});
-      $(".documentary").mouseenter(function(){
-      $(".text").css("color", "blue");
-      $(".textright").css("opacity", "0");
-      $(".textleft").css("opacity", "0");
-});
-    $(".documentary").mouseleave(function(){
-      $(".text").css("color", "black");
-      $(".textright").css("opacity", "1");
-      $(".textleft").css("opacity", "1");
-});
-});
+    });
+
+//    $(".Bushsr").click(function(){
+//      $(".Bushsr1").css("color", "hotpink");
+//      $(".Clinton1").css("opacity", "0");
+//      $(".Bush1").css("opacity", "0");
+//       $(".OJ1").css("opacity", "0");
+//       $(".Obama1").css("opacity", "0");
+//      $(".nine1").css("opacity", "0");   
+//});
+//
+//        $(".nine").click(function(){
+//      $(".nine1").css("color", "orange");
+//      $(".Clinton1").css("opacity", "0");
+//      $(".Bush1").css("opacity", "0");
+//       $(".OJ1").css("opacity", "0");
+//       $(".Obama1").css("opacity", "0");
+//      $(".Bushsr1").css("opacity", "0");   
+//});
+//
+//      $(".Obama").click(function(){
+//      $(".Obama1").css("color", "purple");
+//      $(".Clinton1").css("opacity", "0");
+//      $(".Bush1").css("opacity", "0");
+//       $(".OJ1").css("opacity", "0");
+//       $(".nine1").css("opacity", "0");
+//      $(".Bushsr1").css("opacity", "0");   
+//});
+//
+//
+//});
+//    
 
 
 
